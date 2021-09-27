ce93b7500cfca415fc2aa9064198ee4215c53a47
dwandrew <danwandrew@gmail.com>
2021-09-27 15:34:50 +0100

    added routes folder

--- app.js
+++ app.js
@@ -1,11 +1,9 @@
 const express = require('express')
-
+const {getPosts} = require('./routes/post')
 const app = express()
 const port = 8080
 
-app.get("/", (req, res)=>{
- res.send("testing")
-})
+app.get("/", getPosts)
 
 app.listen(port, ()=>{
     console.log(`A node js API is listening on ${port}`)

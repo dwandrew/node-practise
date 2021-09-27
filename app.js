2804047892c59f21cad9d881a48f18e1795a73c7
dwandrew <danwandrew@gmail.com>
2021-09-27 15:25:38 +0100

    added dev script and nodemon

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
 
 const app = express()
+const port = 8080
 
 app.get("/", (req, res)=>{
  res.send("testing")
 })
 
-app.listen(8080)
+app.listen(port, ()=>{
+    console.log(`A node js API is listening on ${port}`)
+})

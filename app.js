7e66c6ad1062309f286fc77ce9f2352d4bb12d68
dwandrew <danwandrew@gmail.com>
2021-09-27 16:26:49 +0100

    latest changes

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require('express')
-const {getPosts} = require('./routes/post')
+const morgan = require("morgan")
+const postRoutes = require('./routes/post')
 const app = express()
 const port = 8080
 
-app.get("/", getPosts)
+app.use(morgan("dev"))
+
+app.use("/", postRoutes)
 
 app.listen(port, ()=>{
     console.log(`A node js API is listening on ${port}`)
--- routes/post.js
+++ routes/post.js
@@ -1,3 +1,8 @@
-exports.getPosts = (req, res)=>{
-    res.send("testing")
-   }
+const postController = require ("../controllers/post")
+const express = require("express")
+
+const router = express.Router()
+
+router.get("/", postController.getPosts)
+
+module.exports  = router

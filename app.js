18305869032350dbed23ded40ec0b3a87020774f
dwandrew <danwandrew@gmail.com>
2021-09-28 09:40:49 +0100

    mongoose added, would need to install mongodb locally to function

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const morgan = require("morgan")
+const mongoose = require("mongoose")
 const postRoutes = require('./routes/post')
 const app = express()
 const port = 8080
+
+const MONGO_URI = "mongodb://localhost/nodeapi"
+
+mongoose.connect(MONGO_URI, {})
+.then(()=> console.log("DB connected"))
+
+mongoose.connection.on("error", err=> console.log(`DB connection error: ${err}`))
 
 app.use(morgan("dev"))
 
